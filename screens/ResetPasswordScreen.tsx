6a5ce88fc7f814ac0c620959c85fcb55f01fe16d
jfourdachon <jfourdachon@yahoo.com>
2021-04-29 08:53:04 +0200

    wip reset password

---
+++ screens/ResetPasswordScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import ResetPasswordForm from '../components/forms/ResetPasswordForm' 
+
+const ResetPasswordScreen = () => {
+    return (
+        <View>
+            <ResetPasswordForm />
+        </View>
+    )
+}
+
+export default ResetPasswordScreen
+
+const styles = StyleSheet.create({})
