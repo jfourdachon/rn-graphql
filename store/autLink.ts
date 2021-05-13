b1c795401fe83d8ca1051aac7f60b644793bfd92
jfourdachon <jfourdachon@yahoo.com>
2021-05-13 06:32:05 +0200

    split apollo config

---
+++ store/autLink.ts
@@ -0,0 +1,17 @@
+import { createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { API_URL } from "@env";
+import * as SecureStore from 'expo-secure-store';
+
+export const authLink = setContext(async (_, { headers }) => {
+    let token;
+    try {
+        token = await SecureStore.getItemAsync('token')
+    } catch (error) { }
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
---
+++ store/errorLink.ts
@@ -0,0 +1,82 @@
+import { ApolloClient, createHttpLink, fromPromise } from '@apollo/client';
+import { onError } from "@apollo/client/link/error";
+import { API_URL } from "@env";
+import * as SecureStore from 'expo-secure-store';
+import { cache, isLoggedInVar } from './cache'
+import { REFRESH_TOKEN } from './auth/query'
+
+
+let isRefreshing = false
+let pendingRequests: Function[] = []
+
+const setIsRefreshing = (value: boolean) => {
+    isRefreshing = value
+}
+
+const addPendingRequest = (pendingRequest: Function) => {
+    pendingRequests.push(pendingRequest)
+}
+
+const renewTokenApiClient = new ApolloClient({
+    link: createHttpLink({ uri: API_URL }),
+    cache,
+    credentials: 'include',
+})
+
+const resolvePendingRequests = () => {
+    pendingRequests.map((callback) => callback())
+    pendingRequests = []
+}
+
+const getNewToken = async () => {
+    // const oldRenewalToken = localStorage.getItem('renewalToken')
+
+    try {
+        const { data } = await renewTokenApiClient.query({ query: REFRESH_TOKEN })
+        if (data?.refreshToken?.token) {
+
+            await SecureStore.setItemAsync('token', data.refreshToken.token)
+            isLoggedInVar(true)
+        }
+
+    } catch (error) {
+        console.log({ error })
+    }
+
+}
+
+
+export const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    if (graphQLErrors) {
+        graphQLErrors.forEach(({ message }) => {
+            if (message === 'Unauthorized') {
+                if (!isRefreshing) {
+                    setIsRefreshing(true)
+
+                    return fromPromise(
+                        getNewToken().catch(async () => {
+                            resolvePendingRequests()
+                            setIsRefreshing(false)
+                            // TODO logout
+                            await SecureStore.deleteItemAsync('token')
+
+                            return forward(operation)
+                        }),
+                    ).flatMap(() => {
+                        resolvePendingRequests()
+                        setIsRefreshing(false)
+                        return forward(operation)
+                    })
+                } else {
+                    return fromPromise(
+                        new Promise<void>((resolve) => {
+                            addPendingRequest(() => resolve())
+                        }),
+                    ).flatMap(() => {
+                        return forward(operation)
+                    })
+                }
+            }
+        });
+    }
+})
